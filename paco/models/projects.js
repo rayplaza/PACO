40b499391f757b531d4c8b3ce81141c140508fad
Ray Plaza <munditoplaza@gmail.com>
2019-11-29 23:42:23 -0800

    creating user schema

--- paco/models/projects.js
+++ paco/models/projects.js
@@ -1,7 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
+var userSchema = new Schema( {
+    name: String,
+    email: String,
+}, {
+    timestamps: true
+});
+
 var projectSchema = new Schema({
+   user: [userSchema],
    projectName: String,
    profession: String,
    daw: String,
