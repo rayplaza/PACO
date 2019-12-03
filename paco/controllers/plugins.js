24c0953b331f08889170750f0aab1ed67705d7fa
Ray Plaza <munditoplaza@gmail.com>
2019-12-03 09:40:43 -0800

    plugins now populating

--- paco/controllers/plugins.js
+++ paco/controllers/plugins.js
@@ -8,7 +8,8 @@ module.exports = {
 
 function create(req, res) {
     req.body.project = req.params.id
-    Plugin.create(req.body, function(err, ticket) {
+    Plugin.create(req.body, function(err, plugin) {
+        console.log("PLUGIN JUST CREATED: ", plugin)
         res.redirect(`/projects/${req.params.id}`);
     });
 }
--- paco/models/plugin.js
+++ paco/models/plugin.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var pluginSchema = new Schema( {
     manufacturer: String,
     pluginName: String,
+    project: {type: Schema.Types.ObjectId, ref: 'Project'},
     type: {
         type: String,
         enum: ['AmpsPedals', 'Delay', 'Distortion', 'Dynamics', 'EQ', 'Filter', 'Imaging', 'Pitch', 'Reverb', 'Specialized', 'Utility', 'Metering', 'Multi-Effects', 'Other']
