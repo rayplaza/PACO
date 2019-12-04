336cd0db6e3662353a338d282d27f7635fff13ba
Ray Plaza <munditoplaza@gmail.com>
2019-12-03 16:32:55 -0800

    created the update function

--- paco/controllers/plugins.js
+++ paco/controllers/plugins.js
@@ -3,8 +3,29 @@ var Project = require('../models/project');
  
 module.exports = {
   new: newPlugin,
-  create
+  create,
+  edit,
+  update
 };
+
+function update(req, res) {
+    Plugin.update(req.params.id, req.body);
+    res.redirect(`/plugins/${req.params.id}`);
+}
+
+
+function edit (req, res) {
+    let plugs = req.params.pid
+    console.log(plugs)
+    Project.findById(req.params.id, function(err, project) {
+        res.render(`plugins/edit`, {
+            title: "HELLO",
+            plugs,
+            project,
+            user: req.user
+        });
+    });
+}
 
 function create(req, res) {
     req.body.project = req.params.id
--- paco/routes/plugins.js
+++ paco/routes/plugins.js
@@ -4,5 +4,7 @@ var express = require('express');
  
  router.get('/projects/:id/plugins/new', pluginsCtrl.new);
  router.post('/projects/:id/plugins', pluginsCtrl.create);
+ router.get('/projects/:id/plugins/:pid/edit', pluginsCtrl.edit);
+ router.put('/projects/:id/plugins/:pid', pluginsCtrl.update);
  
  module.exports = router;
